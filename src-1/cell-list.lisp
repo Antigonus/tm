140594dd01103f4d8bc8deeb9a310edbd7451f7f
Thomas Walker Lynch <thomas.walker.lynch@reasoningtechnology.com>
2017-08-17 04:16:22 +0000

    array and cell changes

--- src-1/cell-list.lisp
+++ src-1/cell-list.lisp
@@ -7,8 +7,13 @@ Implementation of cell intended for use in a list.
 
 CLOS version of the cons cell.
 
-Subspace and multiple world cells are used in src-cell-1 when more of the tm
-interface is available.
+For supporting subspace, the contents is turned into a tape-array of instances rather than
+an individual instance. 
+
+For Multiple universe, an extra layer of tape-array is added.  Contents becomes a tape-array
+of instances, where each instance is s tape-array for the contents.
+
+Neighbor is a tape array where the instances are links to neighbors.
 
 |#
 
@@ -17,272 +22,134 @@ interface is available.
 ;;--------------------------------------------------------------------------------
 ;; type definition
 ;;
-  (def-type cell-list (cell substrate)
+
+  ;; contents is used directly
+  (def-type cell-list (cell)
     (
-      (contents :initarg :contents :accessor contents)
-      (right-neighbor
-        :init-form ∅
-        :initarg :right-neighbor
-        :accessor right-neighbor
+      (contents 
+        :init-form (make-tape-array)
+        )
+      (neighbor
+        :init-form (make-tape-array)
         )
       ))
 
-  ;; cell with a subspace
-  (def-type cell-list-subspace (cell-list)
-    (
-      (tape ; instances are continuation tapes
-        :init-form ∅
-        )
-      ))
-
+  ;; contents have been forked into multiple universes
+  ;; (links to neighbors are forked individually)
+  (def-type cell-list-multi (cell)())
 
   (defun-typed init ((cell cell-list) instance &optional ➜)
     (destructuring-bind
       (&key
         (➜ok #'echo)
         (➜fail (λ()(error 'bad-init-value)))
-        right-neighbor
         &allow-other-keys
         )
       ➜
-      (w cell instance)
-      (cond
-        (right-neighbor (setf (right-neighbor cell) right-neighbor))
-        (t              (setf (right-neighbor cell) ∅))
-        )
+      (a◨<tape-array> (contents cell) instance)
       [➜ok cell]
       ))
 
 ;;--------------------------------------------------------------------------------
-;; cell functions
+;; read
 ;;
-  ;; we don't provide a cell copy, so one can usually just use eq
-  (defun-typed =<cell> ((context context) (cell-0 cell-list) (cell-1 cell-list) &optional ➜)
-    (declare (ignore context))
-    (destructuring-bind
-      (&key
-        (➜∅ (be ∅))
-        (➜t (be t))
-        &allow-other-keys
-        )
-      ➜
-      (if
-        (∧
-          (eq (right-neighbor cell-0) (right-neighbor cell-1))
-          (eq (contents cell-0) (contents cell-1))
-          )
-        [➜t]
-        [➜∅]
-        )))
-
-  (defun-typed r<cell> (context (cell cell-list) &optional ➜)
-    (declare (ignore context))
-    (destructuring-bind
-      (&key
-        (➜ok #'echo)
-        &allow-other-keys
-        )
-      ➜
-      [➜ok (contents cell)]
-      ))
- 
-  (defun-typed w<cell> ((cell cell-list) instance &optional ➜)
-    (destructuring-bind
-      (&key
-        (➜ok (be t))
-        &allow-other-keys
-        )
-      ➜
-      (setf (contents cell) instance)
-      [➜ok]
-      ))
-
-
-;;--------------------------------------------------------------------------------
-;; cell neighbor functions
-;;
-  ;; by contract distance is positive when neighbor-1 is called
-  ;; differential is a function
-  (defun neighbor-1 (cell differential n &optional ➜)
-    (destructuring-bind
-      (&key
-        (➜ok #'echo)
-        (➜bound-right (λ(cell n)(declare (ignore cell n))(error 'step-from-bound-right)))
-        &allow-other-keys
-        )
-      ➜
-      (⟳(λ(➜again)
-          (cond
-            ((= n 0) [➜ok cell])
-            ((typep cell 'bound-right) [➜bound-right cell n])
-            (t
-              (setf cell [differential cell])
-              (decf n)
-              [➜again]
-              ))))))
-
-  (defun-typed neighbor ((cell cell-list) &optional ➜)
-    (destructuring-bind
-      (&key
-        (➜bad-direction (λ()(error 'bad-direction)))
-        (n 0)
-        &allow-other-keys
-        )
-      ➜
-      (cond
-        ((≥ n 0)
-          (neighbor-1 cell #'right-neighbor n ➜)
-          )
-        (t [➜bad-direction])
-        )))
-
-;;--------------------------------------------------------------------------------
-;; primitive topology manipulation
-;;
-  ;; terminate the connections of a cell so that it can not affect gc
-  ;;
-    (defun cap-right (c)
-      (setf (right-neighbor c) ∅)
-      (to-bound-right c)
-      )
-
-    (def-function-class cap-left (c))
-    (defun-typed cap-left ((c cell-list))
-      (to-bound-left c)
-      )
-
-    (def-function-class cap (c))
-    (defun-typed cap ((c cell-list)) 
-      (setf (right-neighbor c) ∅)
-      (to-cell c)
-      )
-
-  (def-function-class cap<tape> (tape))
-  (defun-typed cap<tape> ((tape cell-list)) 
-    (setf (right-neighbor tape) ∅)
-    (to-empty tape)
+  ;; sid == subspace id
+  (defmacro r<cell> (cell accessor ➜)
+    `(destructuring-bind
+       (&key
+         (➜ok #'echo)
+         (sid 0)
+         &allow-other-keys
+         )
+       ➜
+       [➜ok (r<tape-array> (apply ,accessor ,cell) sid)]
+       ))
+  (defun-typed r<contents> ((cell cell-list) &optional ➜)
+    (r<cell> cell #'contents ➜)
+    )
+  (defun-typed r<neighbor> ((cell cell-list) &optional ➜)
+    (r<cell> cell #'neighbor ➜)
     )
 
-  ;; c0 and c1 are two cells to be connected
-  ;;
-    (def-function-class connect (c0 c1))
-    (defun-typed connect ((c0 link)(c1 link))
-      (setf (right-neighbor c0) c1)
-      )
+  (defmacro r<cell>-multi (cell accessor ➜)
+    `(destructuring-bind
+       (&key
+         (➜ok #'echo)
+         (➜no-universe (λ()(error 'no-universe)))
+         (sid 0)
+         (uid 0) ; if uid is nonzero, then 'parents' must be supplied, enforced by contract with programmer
+         parents 
+         &allow-other-keys
+         )
+       ➜
+       (⟳ (λ(➜again)
+            (setf contents (r<tape-array> (apply ,accessor ,cell) uid))
+            (if
+              contents
+              [➜ok (r<tape-array> contents sid)]
+              (if
+                (= uid 0)
+                [➜no-universe]
+                (progn
+                  (setf uid (get-hash uid parents))
+                  [➜again]
+                  )))))
+       ))
 
-  ;; c0 and c1 are neighbors.  re-routes connections around cell c1
-  ;;
-    (def-function-class extract (c0 c1))
-    (defun-typed extract ((c0 list-bound-left) (c1 list-bound-right))
-      (cap-right c0)
-      (to-solitary c0)
-      (cap c1)
-      )
-    (defun-typed extract ((c0 list-interior) (c1 list-bound-right))
-      (cap-right c0)
-      (cap c1)
-      )
-    (defun-typed extract ((c0 list-bound-left-interior) (c1 list-interior))
-      (let(
-            (c2 (right-neighbor c1))
-            )
-        (connect c0 c2)
-        ;; c0 and c2 each maintains its status
-        (cap c1)
-        ))
-
-  ;; c0 and c1 are neighbors. disconnects them.
-  ;;
-    (def-function-class disconnect (c0 c1))
-    (defun-typed disconnect ((c0 list-bound-left) (c1 list-bound-right-interior))
-      (cap-right c0)
-      (to-solitary c0)
-      (cap-left c1)
-      )
-    (defun-typed disconnect ((c0 list-interior) (c1 list-bound-right-interior))
-      (cap-right c0)
-      (cap-left c1)
-      )
+  (defun-typed r<contents> ((cell cell-list-multi) &optional ➜)
+    (r<cell>-multi cell #'contents ➜)
+    )
+  (defun-typed r<neighbor> ((cell cell-list-multi) &optional ➜)
+    (r<cell>-multi cell #'neighbor ➜)
+    )
 
 ;;--------------------------------------------------------------------------------
-;; topology manipulation, interface implementations
+;; write
 ;;
-  ;; c0 and c2 are neighbors, inserts c1 between them
-  ;;
-    (defun-typed a<cell> ((c0 list-solitary) (c1 cell-list) &optional ➜)
-      (declare (ignore ➜))
-      (cap-right c1)
-      (connect c0 c1)
-      (to-bound-left c0)
-      )
-    (defun-typed a<cell> ((c0 list-bound-right) (c1 cell-list)  &optional ➜)
-      (declare (ignore ➜))
-      (cap-right c1)
-      (connect c0 c1)
-      (to-interior c0)
-      )
-    (defun-typed a<cell> ((c0 list-bound-left-interior) (c1 cell-list)  &optional ➜)
-      (declare (ignore ➜))
-      (let(
-            (c2 (right-neighbor c0))
-            )
-        (connect c0 c1)
-        (connect c1 c2)
-        (to-interior c1)
-        ;; c0 keeps its status
-      ))
+  (defmacro w<cell> (cell instance accessor ➜)
+    `(destructuring-bind
+       (&key
+         (➜ok (be t))
+         (sid 0)
+         &allow-other-keys
+         )
+       ➜
+       (w<tape-array> (,accessor ,cell) instance sid)
+       ))
+  (defun-typed w<contents> ((cell cell-list) instance &optional ➜)
+    (w<cell> cell instance 'contents ➜)
+    )
+  (defun-typed w<neighbor> ((cell cell-list) instance &optional ➜)
+    (w<cell> cell instnace 'neighbor ➜)
+    )
 
-  ;; Deletes the right neighbor cell.
-  ;; bound-right handled on the interface.
-  ;; solitary, though this has the same behavior for all cells, had to be put 
-  ;; here as otherwise CLOS wold choose list-bound-left-interior as being more specific
-  ;;
-    (defun-typed d<cell> ((cell list-solitary) &optional ➜)
-      (destructuring-bind
-        (&key
-          (➜bound-right (λ()(error 'dealloc-on-bound-right)))
-          &allow-other-keys
-          )
-        ➜
-        [➜bound-right]
-        ))
-    (defun-typed d<cell> ((c0 list-bound-left-interior) &optional ➜)
-      (destructuring-bind
-        (&key
-          (➜ok #'echo)
-          &allow-other-keys
-          )
-        ➜
-        (let(
-              (c1 (right-neighbor c0))
-              )
-          (extract c0 c1)
-          [➜ok c1]
-          )))
-
-  ;; c0 bound-right case handled on the interface.
-  ;; solitary, though this has the same behavior for all cells, had to be put 
-  ;; here as otherwise CLOS wold choose list-bound-left-interior as being more specific
-  ;;
-    (defun-typed d+<cell> ((cell list-solitary) &optional ➜)
-      (destructuring-bind
-        (&key
-          (➜bound-right (λ()(error 'dealloc-on-bound-right)))
-          &allow-other-keys
-          )
-        ➜
-        [➜bound-right]
-        ))
-    (defun-typed d+<cell> ((c0 list-bound-left-interior) &optional ➜)
-      (destructuring-bind
-        (&key
-          (➜ok #'echo)
-          &allow-other-keys
-          )
-        ➜
-        (let(
-              (c1 (right-neighbor c0))
-              )
-          (disconnect c0 c1)
-          [➜ok c1]
-          )))
+  (defmacro wa<cell>-multi (cell instance accessor operator ➜)
+    `(destructuring-bind
+       (&key
+         (➜ok #'echo)
+         (sid 0)
+         (uid 0)
+         parents
+         &allow-other-keys
+         )
+       ➜
+       (expand<tape-array> (,accessor ,cell) uid)
+       (,operator
+         (r<tape-array> (,accessor ,cell) uid)
+         instance
+         sid
+         )
+       [➜ok]
+       ))
+  (defun-typed w<contents> ((cell cell-list-multi) instance &optional ➜)
+    (wa<cell> cell instance 'contents 'w<tape-array> ➜)
+    )
+  (defun-typed w<neighbor> ((cell cell-list-multi) instance &optional ➜)
+    (w<cell> cell instance 'neighbor 'w<tape-array> ➜)
+    )
+  (defun-typed a<contents> ((cell cell-list-multi) instance &optional ➜)
+    (wa<cell> cell instance 'contents 'a◨<tape-array> ➜)
+    )
+  (defun-typed a<neighbor> ((cell cell-list-multi) instance &optional ➜)
+    (w<cell> cell instance 'neighbor 'a◨<tape-array> ➜)
+    )
---
+++ src-1/tape-array.lisp
@@ -0,0 +1,73 @@
+#|
+Copyright (c) 2017 Thomas W. Lynch and Reasoning Technology Inc.
+Released under the MIT License (MIT)
+See LICENSE.txt
+
+A tape-array.
+
+Typically starts out with a length of zero or one, and then grows slowly via a◨.
+
+Designed for storage critical applications, as it doesn't take up any more storage
+than necessary.  This makes growing a bit expensive, as the whole array is copied
+each time a cell is added.
+
+Common Lisp knows the length of an array, and that information must be stored somewhere.
+Hence I would like expand the tape from being a single instance, a pair instances held in
+a struct, perhaps a triple held in a struct, then an array.  To support such a progression
+with CLOS, we must distinguish a pair, struct, or an array when they appear as the single
+instance, from the pair, struct, or array we use for expansion. I don't see a reasonable
+way to do this, as common Lisp isn't giving me a way to alias a type, and if I make a wrapper
+type it looks like there is a yet another layer of indirection or worse.  I suppose one could
+type-of, create a new type, and change-type ... nah.
+
+One good thing about making tape array an array is that nil is not a valid value.  I use
+this in the cell list to distringuish when there are no contents for a given universe id, and
+thus we should check the parents table for an inherited value.
+
+In this implementation we just use a simple array and copy and expand as needed.  In the
+single instance case, where the single instance is a number or a referece, we pay big with
+100% overhead.
+
+
+|# 
+
+(in-package #:tm)
+
+(defun make<tape-array> (&optional (length 0) (init ∅)) (make-array length :initial-element init))
+
+(demacro expand<tape-array> (tape-array max-address)
+  `(let(
+         (length (length ,tape-array))
+         )
+     (when
+       (≥ ,max-address length)
+       (let(
+             (new-tape-array (make-array (1+ ,max-address)))
+             )
+         (loop for address below length do
+           (setf (aref new-tape-array address) (aref ,tape-array))
+           )
+         (loop for address upto ,max-address do
+           (setf (aref new-tape-array address) ∅)
+           )))))
+
+;; hopefully the compiler is smart enough to eliminate the array access in the default case
+(defmacro r<tape-array> (tape-array &optional (address 0))
+  `(aref ,tape-array ,address)
+  )
+(defmacro w<tape-array> (tape-array instance &optional (address 0))
+  `(setf (aref ,tape-array ,address) ,instance)
+  )
+
+;; need to add the ➜ code because we can have an allocation error
+(defmacro a◨<tape-array> (tape-array instance &optional ➜)
+  `(let*(
+          (length         (length ,tape-array))
+          (new-tape-array (make-array (1+ ,length)))
+          )
+     (loop for address below length do
+       (setf (aref new-tape-array address) (aref ,tape-array address))
+       )
+     (setf (aref ,new-tape-array length) ,instance)
+     (setf ,tape-array new-tape-array)
+     ))
